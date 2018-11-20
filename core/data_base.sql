9236f6f7687a54bc251ef5632137d52d51db4d46
Jonathan <jonathan@appserver.proconpb.local>
2018-11-20 14:34:03 -0300

    Adicionando sweetalert2, corrigindo bugs

--- core/data_base.sql
+++ core/data_base.sql
@@ -11,22 +11,22 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema gincana2017
+-- Schema gincana
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `gincana2017` ;
+DROP SCHEMA IF EXISTS `gincana` ;
 
 -- -----------------------------------------------------
--- Schema gincana2017
+-- Schema gincana
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `gincana2017` DEFAULT CHARACTER SET utf8 ;
-USE `gincana2017` ;
+CREATE SCHEMA IF NOT EXISTS `gincana` DEFAULT CHARACTER SET utf8 ;
+USE `gincana` ;
 
 -- -----------------------------------------------------
--- Table `gincana2017`.`equipe`
+-- Table `gincana`.`equipe`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `gincana2017`.`equipe` ;
+DROP TABLE IF EXISTS `gincana`.`equipe` ;
 
-CREATE TABLE IF NOT EXISTS `gincana2017`.`equipe` (
+CREATE TABLE IF NOT EXISTS `gincana`.`equipe` (
   `id_equipe` INT(11) NOT NULL AUTO_INCREMENT,
   `nome_equipe` VARCHAR(45) CHARACTER SET 'utf8' NULL DEFAULT NULL,
   PRIMARY KEY (`id_equipe`),
@@ -38,11 +38,11 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `gincana2017`.`perguntas`
+-- Table `gincana`.`perguntas`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `gincana2017`.`perguntas` ;
+DROP TABLE IF EXISTS `gincana`.`perguntas` ;
 
-CREATE TABLE IF NOT EXISTS `gincana2017`.`perguntas` (
+CREATE TABLE IF NOT EXISTS `gincana`.`perguntas` (
   `id_pergunta` INT(11) NOT NULL AUTO_INCREMENT,
   `pergunta` TEXT NOT NULL,
   PRIMARY KEY (`id_pergunta`))
@@ -52,11 +52,11 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `gincana2017`.`gabarito`
+-- Table `gincana`.`gabarito`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `gincana2017`.`gabarito` ;
+DROP TABLE IF EXISTS `gincana`.`gabarito` ;
 
-CREATE TABLE IF NOT EXISTS `gincana2017`.`gabarito` (
+CREATE TABLE IF NOT EXISTS `gincana`.`gabarito` (
   `id_gabarito` INT(11) NOT NULL AUTO_INCREMENT,
   `a` TEXT NOT NULL,
   `b` TEXT NOT NULL,
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `gincana2017`.`gabarito` (
   PRIMARY KEY (`id_gabarito`),
   CONSTRAINT `fk_gabarito_1`
     FOREIGN KEY (`id_gabarito`)
-    REFERENCES `gincana2017`.`perguntas` (`id_pergunta`)
+    REFERENCES `gincana`.`perguntas` (`id_pergunta`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -75,11 +75,11 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `gincana2017`.`respostas`
+-- Table `gincana`.`respostas`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `gincana2017`.`respostas` ;
+DROP TABLE IF EXISTS `gincana`.`respostas` ;
 
-CREATE TABLE IF NOT EXISTS `gincana2017`.`respostas` (
+CREATE TABLE IF NOT EXISTS `gincana`.`respostas` (
   `resposta` CHAR(1) NOT NULL,
   `equipe_id_equipe` INT(11) NOT NULL,
   `perguntas_id_pergunta` INT(11) NOT NULL,
@@ -88,12 +88,12 @@ CREATE TABLE IF NOT EXISTS `gincana2017`.`respostas` (
   INDEX `fk_respostas_2_idx` (`perguntas_id_pergunta` ASC),
   CONSTRAINT `fk_respostas_1`
     FOREIGN KEY (`equipe_id_equipe`)
-    REFERENCES `gincana2017`.`equipe` (`id_equipe`)
+    REFERENCES `gincana`.`equipe` (`id_equipe`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_respostas_2`
     FOREIGN KEY (`perguntas_id_pergunta`)
-    REFERENCES `gincana2017`.`perguntas` (`id_pergunta`)
+    REFERENCES `gincana`.`perguntas` (`id_pergunta`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
--- core/gincana_respostas.sql
+++ core/gincana_respostas.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `gincana2017` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `gincana2017`;
+CREATE DATABASE  IF NOT EXISTS `gincana` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gincana`;
 -- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
 --
--- Host: localhost    Database: gincana2017
+-- Host: localhost    Database: gincana
 -- ------------------------------------------------------
 -- Server version	5.7.20-0ubuntu0.16.04.1
 
@@ -40,11 +40,11 @@ CREATE TABLE `respostas` (
 -- Dumping data for table `respostas`
 --
 
-LOCK TABLES `respostas` WRITE;
+-- LOCK TABLES `respostas` WRITE;
 /*!40000 ALTER TABLE `respostas` DISABLE KEYS */;
-INSERT INTO `respostas` VALUES ('b',1,1,0),('c',3,1,1),('b',1,2,1);
+-- INSERT INTO `respostas` VALUES ('b',1,1,0),('c',3,1,1),('b',1,2,1);
 /*!40000 ALTER TABLE `respostas` ENABLE KEYS */;
-UNLOCK TABLES;
+-- UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
